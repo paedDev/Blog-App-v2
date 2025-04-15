180d282188d840af30fc5a0808bcb99893737e7e
Jan Noel S. Paed <jannoelpaed27@gmail.com>
2025-04-16 00:32:31 +0800

    added fetch in the blog controller

--- server/controller/blog-controller.js
+++ server/controller/blog-controller.js
@@ -1,8 +1,21 @@
 import mongoose from "mongoose";
-import blog from "../model/blog.js";
+import Blog from "../model/blog.js";
 
 //lets do the all this following :
 //fetch
 //add
 //delete
 //update
+
+export default fetchListOfBlogs = async (req, res) => {
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!blogList) {
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
+  return res.status(200).json({ blogList });
+};
