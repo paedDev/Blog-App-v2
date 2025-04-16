aced9d10da1184354a016dd1074c79f332b1faef
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-04-16 22:22:17 +0800

    done with the backend functionality

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ import express from "express";
 import cors from "cors";
 import connectDb from "./db/index.js";
 connectDb();
+import blogRouter from "./route/blog-route.js";
 const app = express();
 const PORT = 5000;
 app.use(cors());
 
 app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
+app.use("/api", (req, res) => {
+  res.send("Testing purposes");
+});
 app.listen(PORT, (req, res) => {
   console.log(`Listening to port ${PORT}`);
 });
