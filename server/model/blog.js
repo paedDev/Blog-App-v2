6239c66e7c8d7df39fb5fdb6a624f2a80cf844ac
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-04-19 22:40:40 +0800

    added timestamp in the schema

--- server/model/blog.js
+++ server/model/blog.js
@@ -1,19 +1,24 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const blogSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("MyBlog", blogSchema);
